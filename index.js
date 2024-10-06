dbcb0d0a287903842321fc49ddc32662b54ec1ec
Jason Lee <jaeho52lee@gmail.com>
2024-10-05 22:43:58 -0500

    placed mongo uri in environment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const {MongoClient} = require("mongodb"); 
 
 async function main() {
-    const uri = process.env.MONGO_URI
-    const client = new MongoClient(uri)
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri);
     const mongoose = require("mongoose"); 
     mongoose.connect("mongodb://localhost:27017/magesDB");
 
