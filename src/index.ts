b624fa3f311e8c1ccf1f42f07a4ae21205a83e67
Jason Lee <jaeho52lee@gmail.com>
2024-10-06 11:38:52 -0500

    added route path to root

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,10 @@ app.use(express.urlencoded({ extended: true })); // Parses incoming requests wit
 
 app.use("/user", userRouter); 
 
+app.get("/", (req: Request, res: Response) => {
+    res.send("Server is up!");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
