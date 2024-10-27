2240e762860c1f98d0e01d380b4fbe69864c5efd
Jason Lee <jaeho52lee@gmail.com>
2024-10-27 13:19:23 -0500

    modified prediction controller

--- src/controllers/predictionController.ts
+++ src/controllers/predictionController.ts
@@ -3,12 +3,12 @@ import axios from "axios";
 
 const generatePrediction = async (req: Request, res: Response) => {
     try {
-        const gameState = req.body.gameState; 
+        const { gameState } = req.body; 
         const response = await axios.post(`${process.env.AI_URL}/predict`, {
             game_state: gameState
         });
         
-        res.json({ bestMove: response.data.best_move });
+        res.json({ cellInd: response.data.best_move.cellInd, gridInd: response.data.best_move.gridInd });
     } catch (err) {
         res.status(500).send('Failed to get prediction');
     }
